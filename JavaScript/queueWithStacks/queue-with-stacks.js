dab951d0cd42644ae50e7b514bcc4c93b43cb766
James Denton <sgtbelly@gmail.com>
2018-11-04 01:24:32 -0800

    worked on the code

--- JavaScript/queueWithStacks/queue-with-stacks.js
+++ JavaScript/queueWithStacks/queue-with-stacks.js
@@ -4,23 +4,28 @@ var Stack2 = [];
 
 // implement enqueue method by using only stacks
 // and the push and pop functions
-function Enqueue(element) {
-  Stack1.push(element);
-}
+Class PseudoQueue{
+
+  this.Enqueue(element){
+    Stack1.push(element);
+  }
 
 // implement dequeue method by pushing all elements
 // from stack 1 into stack 2, which reverses the order
 // and then popping from stack 2
-function Dequeue() {
-  if (Stack2.length === 0) {
-    if (Stack1.length === 0) { return 'Cannot dequeue because queue is empty'; }
-    while (Stack1.length > 0) {
-      var p = Stack1.pop();
-      Stack2.push(p);
+
+  this.Dequeue() {
+    if (Stack2.length === 0) {
+      if (Stack1.length === 0) { return 'Cannot dequeue because queue is empty'; }
+      while (Stack1.length > 0) {
+        var p = Stack1.pop();
+        Stack2.push(p);
+      }
     }
+    return Stack2.pop();
   }
-  return Stack2.pop();
 }
+
 
 Enqueue('a');
 Enqueue('b');
