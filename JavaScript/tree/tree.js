cb03a226e6f7ed2665b66a189fb72f795646e12b
James Denton <sgtbelly@gmail.com>
2018-11-17 20:35:50 -0800

    started lab

---
+++ JavaScript/tree/tree.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor(root = null) {
+
+  }
+
+}
+
+class BinarySearchTree{
+
+}
